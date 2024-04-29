acfbe466788dfde7b737080248eee1d3403d649e
zeak-z <153205102+zeak-z@users.noreply.github.com>
2024-04-28 17:13:25 -0700

    Add files via upload

--- config/rofi/rofi-time.rs
+++ config/rofi/rofi-time.rs
@@ -1,23 +1,23 @@
 use std::process::Command;
 
 fn main() {
-    // Get the current system time in 12-hour format
-    let time = format_system_time();
+    // Get the current system time and date
+    let datetime = format_system_datetime();
 
-    // Output the time directly to stdout with HTML tags for styling
+    // Output the time and date directly to stdout with HTML tags for styling
     println!(
         "\0no-custom\x1ftrue\n\0markup-rows\x1ftrue\n\0prompt\x1f{}\n\0combos\x1fshift+Return",
-        time
+        datetime
     );
 }
 
-fn format_system_time() -> String {
-    let formatted_time = Command::new("date")
-        .args(&["+%I:%M %p"])
+fn format_system_datetime() -> String {
+    let formatted_datetime = Command::new("date")
+        .args(&["+%I:%M |%p| %m/%d"]) // Changed the format here
         .output()
         .expect("Failed to execute command")
         .stdout;
 
-    String::from_utf8(formatted_time).expect("Invalid UTF-8").trim().to_string()
+    String::from_utf8(formatted_datetime).expect("Invalid UTF-8").trim().to_string()
 }
 
